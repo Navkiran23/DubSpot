24c6db15cb25b67d0d3421823ac61588d66c99ff
darren <darrenh1@cs.washington.edu>
2023-05-01 20:25:05 -0700

    reformat js

--- FrontEnd/Calendar.js
+++ FrontEnd/Calendar.js
@@ -12,52 +12,52 @@ let offset = 0;
 // returns an array of 7 consecutive-day Date() objects, ranging from Sun-Sat, and int offset weeks
 // from the current week, or int offset + 1 weeks from the current week if it is currently Sat.
 function calculateWeek() {
-    let today = new Date();
-    const dayOfWeek = today.getDay();
-    const sunday = new Date(today.setDate(today.getDate() - dayOfWeek + (dayOfWeek === 6 ? 7 : 0) + (offset * 7)));
-    let weekArray = [new Date(sunday)];
-    for (let i = 0; i < 6; i++) {
-        weekArray.push(new Date(sunday.setDate(sunday.getDate() + 1)));
-    }
-    return weekArray;
+  let today = new Date();
+  const dayOfWeek = today.getDay();
+  const sunday = new Date(today.setDate(today.getDate() - dayOfWeek + (dayOfWeek === 6 ? 7 : 0) + (offset * 7)));
+  let weekArray = [new Date(sunday)];
+  for (let i = 0; i < 6; i++) {
+    weekArray.push(new Date(sunday.setDate(sunday.getDate() + 1)));
+  }
+  return weekArray;
 }
 
 // updates the date range and the headings for the days
 function updateHeading(weekArray) {
-    heading.innerHTML = `<strong>${weekArray[0].getDate()} ${months[weekArray[0].getMonth()]} 
+  heading.innerHTML = `<strong>${weekArray[0].getDate()} ${months[weekArray[0].getMonth()]} 
             –  ${weekArray[6].getDate()} ${months[weekArray[6].getMonth()]}</strong> 
             ${weekArray[6].getFullYear()}`;
-    let today = new Date();
-    let htmlString = "<thead><tr><th class=\"headcol\"></th>";
-    for (let i = 0; i < 7; i++) {
-        if (today.getDate() === weekArray[i].getDate() && today.getMonth() === weekArray[i].getMonth() &&
-                today.getFullYear() === weekArray[i].getFullYear()) {
-            htmlString += `<th class="today">${daysOfWeek[i]}, ${weekArray[i].getDate()}</th>`;
-        } else {
-            htmlString += `<th>${daysOfWeek[i]}, ${weekArray[i].getDate()}</th>`;
-        }
+  let today = new Date();
+  let htmlString = "<thead><tr><th class=\"headcol\"></th>";
+  for (let i = 0; i < 7; i++) {
+    if (today.getDate() === weekArray[i].getDate() && today.getMonth() === weekArray[i].getMonth() &&
+        today.getFullYear() === weekArray[i].getFullYear()) {
+      htmlString += `<th class="today">${daysOfWeek[i]}, ${weekArray[i].getDate()}</th>`;
+    } else {
+      htmlString += `<th>${daysOfWeek[i]}, ${weekArray[i].getDate()}</th>`;
     }
-    dayHeadings.innerHTML = htmlString;
+  }
+  dayHeadings.innerHTML = htmlString;
 }
 
 // event listener for changing calendar view -1 week
 buttonLeft.addEventListener("click", function() {
-    offset--;
-    let weekArray = calculateWeek();
-    updateHeading(weekArray);
+  offset--;
+  let weekArray = calculateWeek();
+  updateHeading(weekArray);
 });
 
 // event listener for changing calendar view +1 week
 buttonRight.addEventListener("click", function() {
-    offset++;
-    let weekArray = calculateWeek();
-    updateHeading(weekArray);
+  offset++;
+  let weekArray = calculateWeek();
+  updateHeading(weekArray);
 });
 
 todayButton.addEventListener("click", function() {
-    offset = 0;
-    let weekArray = calculateWeek();
-    updateHeading(weekArray);
+  offset = 0;
+  let weekArray = calculateWeek();
+  updateHeading(weekArray);
 });
 
 // Runs this immediately, sets date automatically
