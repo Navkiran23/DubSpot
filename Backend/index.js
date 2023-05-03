2f0a98afcc62832dee221d0450b98b420eabf90a
darren <darrenh1@cs.washington.edu>
2023-05-03 02:19:06 -0700

    Connected webpages to server, changed src files to relative

--- Backend/index.js
+++ Backend/index.js
@@ -5,10 +5,28 @@ app.use(express.static('FrontEnd'));
 const root = path.join(__dirname, '..');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(root, 'FrontEnd', 'DubSpotWeb.html'));
+  res.sendFile(path.join(root, 'FrontEnd', 'DubSpotWeb.html'))
 })
 
+app.get('/Calendar', (req, res) => {
+  res.sendFile(path.join(root, 'FrontEnd', 'DubSpotCalendar.html'))
+})
 
+app.get('/CourseFinder', (req, res) => {
+  res.sendFile(path.join(root, 'FrontEnd', 'DubSpotCourseFinder.html'))
+})
+
+app.get('/Help', (req, res) => {
+  res.sendFile(path.join(root, 'FrontEnd', 'DubSpotHelp.html'))
+})
+
+app.get('/About', (req, res) => {
+  res.sendFile(path.join(root, 'FrontEnd', 'DubSpotAbout.html'))
+})
+
+app.get('/Profile', (req, res) => {
+  res.sendFile(path.join(root, 'FrontEnd', 'DubSpotProfile.html'))
+})
 
 app.listen(3000, () => {
   console.log('Server started on port 3000')
