3980fe22f5800afb8306598e1094c0853840738d
darren <darrenh1@cs.washington.edu>
2023-05-03 01:50:25 -0700

    updated server

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const path = require('path');
 const app = express()
+app.use(express.static('FrontEnd'));
+const root = path.join(__dirname, '..');
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!')
+  res.sendFile(path.join(root, 'FrontEnd', 'DubSpotWeb.html'));
 })
 
+
+
 app.listen(3000, () => {
-    console.log('Server started on port 3000')
+  console.log('Server started on port 3000')
 })
