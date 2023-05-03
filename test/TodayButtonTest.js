cecba3ded1a4da3461d680befcd4f72b17220867
Navkiran <100175211+Navkiran23@users.noreply.github.com>
2023-05-02 17:06:59 -0700

    Create TodayButtonTest.js
    Added a test to see if it matches todays date!

---
+++ test/TodayButtonTest.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+describe('TodayTest', function() {
+  describe('Today', function() {
+    it('should equal todays date when loading', function() {
+      var result = today.getDate();
+      assert.equal(today.getDate(), May 2nd);
+    });
+  });
+});
