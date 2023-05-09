275193bb3836cf6c6ec0657abceeb313467e1918
darren <darrenh1@cs.washington.edu>
2023-05-09 03:24:10 -0700

    added GetCoursesTest.js, skipped other tests and renamed TodayButtonTest.js to CalendarTest.js

--- test/GetPasswordTest.js
+++ test/GetPasswordTest.js
@@ -3,7 +3,7 @@ const document = require('../FrontEnd/DubspotWeb.html')
 const assert = require('assert')
 describe('PasswordTest', function () {
   describe('Password', function () {
-    it('should return the hardcoded password', function () {
+    it.skip('should return the hardcoded password', function () {
       assert.equal(document.getElementById("password").value, admin)
     })
   })
--- test/GetUserNameTest.js
+++ test/GetUserNameTest.js
@@ -3,7 +3,7 @@ const document = require('../FrontEnd/DubspotWeb.html')
 const assert = require('assert')
 describe('UsernameTest', function () {
   describe('Username', function () {
-    it('should return the hardcoded username', function () {
+    it.skip('should return the hardcoded username', function () {
       assert.equal(document.getElementById("email").value, "admin@gmail.com")
     })
   })
